c46e0a0a19074ae11a8edf7e9608eba829ef07c1
bygunel <55777855+bygunel@users.noreply.github.com>
2022-05-19 21:06:45 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,23 +1,12 @@
 <?php
 ob_start();
-session_start();
-
-ini_set('error_reporting', E_ALL);
-
-date_default_timezone_set('Europe/istanbul');
-setlocale(LC_ALL, 'tr_TR.UTF-8', 'tr_TR', 'tr', 'turkish');
-
-if (basename($_SERVER['PHP_SELF']) == basename(__FILE__)) {
-	exit(' Erişim Engellendi.');
-};
-
 $config = [
 	'host'      => 'localhost',
 	'driver'    => 'mysql',
-	'database'  => 'realist_portal',
+	'database'  => 'doviz',
 	'port'      => '3306',
-	'username'  => 'realist_portal',
-	'password'  => 'ra9Y03ZUy',
+	'username'  => 'root',
+	'password'  => '',
 	'charset'   => 'utf8',
 	'collation' => 'utf8_turkish_ci',
 	'prefix'    => ''
@@ -37,5 +26,3 @@ try {
 } catch (PDOException $e) {
 	echo "Bağlantı hatası : " . $e->getMessage();
 }
-
-$ip	=	$_SERVER["REMOTE_ADDR"];
